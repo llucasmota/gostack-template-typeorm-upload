d242deedafd612f3a56ab6a08e446a3971f4c027
Lucas Mota <lucas.o.mota@gmail.com>
2020-06-10 09:24:24 -0300

    Adicionando implementação para criação de uma categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import CreateCategoryService from '../services/CreateCategoryService';
 
 const categories = Router();
 
+const createCategoryService = new CreateCategoryService();
+
 categories.post('/', async (request, response) => {
-  const { id, title } = request.body;
+  const { title } = request.body;
+  const category = await createCategoryService.execute(title);
+  return response.json(category);
 });
+export default categories;
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,21 @@
+import { getRepository } from 'typeorm';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+export default class CreateCategoryService {
+  public async execute(title: string): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+    const checkCategoryExists = await categoryRepository.findOne({
+      where: {
+        title,
+      },
+    });
+    if (checkCategoryExists) {
+      throw new AppError('Category already exists');
+    }
+    const category = categoryRepository.create({ title });
+    await categoryRepository.save(category);
+
+    return category;
+  }
+}
